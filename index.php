3991881f80edff72385eaf797a2ca094beb7f6bb
Nzingani <isabelnzingani@outlook.com>
2018-07-31 08:47:59 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>P5ex6</title>
+  </head>
+  <body>
+    <p>
+  <?php
+       $departement = array ('02' => 'Aisne', '59' => 'Nord',' 60' => 'Oise',
+           '62' => 'Pas-de-Calais', '80' => 'Somme');
+   echo $departement['59'];
+  ?>
+</p>
+</body>
